d4025ff5d331f01b6a712f8337b35fb7db781792
“parkhyunjae”  <phj2131@naver.com>
2021-07-26 21:18:52 +0900

    pinter v1.1.13

--- pinter/settings.py
+++ pinter/settings.py
@@ -132,6 +132,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')     ##추가 11강
+
+STATICFILES_DIRS = [        ##추가함 11강
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

f7bf7b53eeb52e8fe381e85164921e414e9a44ef
“parkhyunjae”  <phj2131@naver.com>
2021-07-28 18:46:11 +0900

    pinter v 1.35end

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    ## 35강 ,아티클 모델설정
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    #project = models.ForeignKey(project, on_delete=models.SET_NULL, related_name='article', null=True)
+
+
+    title = models.CharField(max_length=200, null=True)     ## 제목 !
+    image = models.ImageField(upload_to='article/', null=False)     ## 이미지
+    content = models.TextField(null=True)       ## 내용 !!
+
+    created_at = models.DateField(auto_now_add=True, null=True)     ## 언제 만들어 졌는지 확인가능 (생성된 시간 저장됨)
+
--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from articleapp.views import ArticleDetailView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
+
+app_name = 'articleapp'
+
 urlpatterns = [
     ## 장고 템플릿으로 바로 만들수 있다
-    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list')
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
+
+    path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
+
 ]
